9b549fc886f6e2b158bd8115184e618cbd9eaefa
Anduin Xue <anduin@aiursoft.com>
2023-05-13 09:30:20 +0000

    Full UT.

--- src/Parser.FFmpeg/FFmpegHandler.cs
+++ src/Parser.FFmpeg/FFmpegHandler.cs
@@ -6,15 +6,15 @@ namespace Anduin.Parser.FFmpeg;
 
 public class FFmpegHandler : ServiceCommandHandler<FFmpegEntry, StartUp>
 {
-    private Option<bool> _useGpu = new Option<bool>(
+    private readonly Option<bool> _useGpu = new(
         getDefaultValue: () => false,
         aliases: new[] { "--gpu", "-g" },
         description: "Use NVIDIA GPU to speed up parsing. Only if you have an NVIDIA GPU attached.");
 
-    private Option<int> _crf = new Option<int>(
+    private readonly Option<int> _crf = new(
         getDefaultValue: () => 20,
         aliases: new[] { "--crf", "-c" },
-        description: "The range of the CRF scale is 0–51, where 0 is lossless (for 8 bit only, for 10 bit use -qp 0), 20 is the default, and 51 is worst quality possible.");
+        description: "The range of the CRF scale is 0–51, where 0 is loss-less (for 8 bit only, for 10 bit use -qp 0), 20 is the default, and 51 is worst quality possible.");
 
     public override string Name => "ffmpeg";
 
@@ -40,7 +40,7 @@ public class FFmpegHandler : ServiceCommandHandler<FFmpegEntry, StartUp>
             _crf);
     }
 
-    public Task ExecuteOverride(string path, bool dryRun, bool verbose, bool useGpu, int crf)
+    private Task ExecuteOverride(string path, bool dryRun, bool verbose, bool useGpu, int crf)
     {
         var services = BuildServices(verbose);
         services.AddSingleton(new FFmpegOptions { UseGpu = useGpu, Crf = crf });
---
+++ tests/Parser.Tests/IntegrationTests.cs
@@ -0,0 +1,90 @@
+﻿using System.CommandLine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
+using Anduin.Parser.Core.Framework;
+using Anduin.Parser.FFmpeg;
+
+namespace Parser.Tests;
+
+[TestClass]
+public class IntegrationTests
+{
+    private readonly RootCommand _program;
+    private readonly string _testVideo;
+
+    public IntegrationTests()
+    {
+        var descriptionAttribute = (Assembly.GetEntryAssembly() ?? Assembly.GetExecutingAssembly()).GetCustomAttribute<AssemblyDescriptionAttribute>()?.Description;
+
+        this._testVideo = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "assets", "test_video.mp4");
+        this._program = new RootCommand(descriptionAttribute ?? "Unknown usage.")
+            .AddGlobalOptions()
+            .AddPlugins(
+                new FFmpegPlugin()
+            );
+    }
+
+    [TestMethod]
+    public async Task InvokeHelp()
+    {
+        var result = await _program.InvokeAsync(new[] { "--help" });
+        Assert.AreEqual(0, result);
+    }
+
+    [TestMethod]
+    public async Task InvokeVersion()
+    {
+        var result = await _program.InvokeAsync(new[] { "--version" });
+        Assert.AreEqual(0, result);
+    }
+
+    [TestMethod]
+    public async Task InvokeUnknown()
+    {
+        var result = await _program.InvokeAsync(new[] { "--wtf" });
+        Assert.AreEqual(1, result);
+    }
+
+    [TestMethod]
+    public async Task InvokeWithoutArg()
+    {
+        var result = await _program.InvokeAsync(Array.Empty<string>());
+        Assert.AreEqual(1, result);
+    }
+
+    [TestMethod]
+    public async Task InvokeFFmpegWithoutArg()
+    {
+        var result = await _program.InvokeAsync(new[] { "ffmpeg" });
+        Assert.AreEqual(1, result);
+    }
+
+    [TestMethod]
+    public async Task InvokeFFmpegWithTemp()
+    {
+        // Prepare
+        var tempFolder = Path.Combine(Path.GetTempPath(), $"Parser-UT-{Guid.NewGuid()}");
+        var tempFile = Path.Combine(tempFolder, "test-video.mp4");
+        if (!Directory.Exists(tempFolder))
+        {
+            Directory.CreateDirectory(tempFolder);
+        }
+        File.Copy(_testVideo, tempFile);
+
+        // Run
+        var result = await _program.InvokeAsync(new[]
+        {
+            "ffmpeg",
+            "--path",
+            tempFolder
+        });
+
+        // Assert
+        Assert.AreEqual(0, result);
+        Assert.IsTrue(File.Exists(Path.Combine(tempFolder, "test-video_265.mp4")));
+        Assert.IsFalse(File.Exists(tempFile));
+
+        // Clean
+        Directory.Delete(tempFolder, true);
+    }
+}
